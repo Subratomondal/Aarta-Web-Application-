83c52edee41adcbaa60e8c7e5819a1a985b11134
Subrato Mondal <subratomondal374@gmail.com>
2025-07-09 18:08:06 +0530

    some updates

--- Aarta/Aarta/settings.py
+++ Aarta/Aarta/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    # 'users',
     'products',
     'orders',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
