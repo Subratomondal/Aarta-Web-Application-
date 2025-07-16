6de342ceeed58e4f48d063aeb6fd9d7c97c31006
Subrato Mondal <subratomondal374@gmail.com>
2025-07-16 20:33:16 +0530

    now admin can see which item is ordered along with address details

--- Aarta/orders/admin.py
+++ Aarta/orders/admin.py
@@ -1,12 +1,21 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):  # Or use admin.StackedInline
+    model = OrderItem
+    extra = 0
+    readonly_fields = ('product', 'quantity', 'price')
+    can_delete = False
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'buyer', 'full_name', 'phone_number', 'city', 'state',
-        'postal_code', 'status', 'total_amount', 'created_at','address'
+        'postal_code', 'status', 'total_amount', 'created_at'
     )
     search_fields = ('buyer__username', 'full_name', 'city', 'state')
     readonly_fields = ('buyer', 'full_name', 'phone_number', 'address', 'city', 'state', 'postal_code', 'total_amount')
+
+    inlines = [OrderItemInline]
